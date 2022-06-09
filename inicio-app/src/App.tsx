cce86d277b2e4b1c00986309359c76a108d114ec
Nathan-Delanhese <nnathanh3@gmail.com>
2022-06-09 11:58:12 -0300

    inicio curso

--- inicio-app/src/App.tsx
+++ inicio-app/src/App.tsx
@@ -1,32 +1,35 @@
 import { useState } from "react";
 import { Tweet } from "./components/Tweet";
-
+import './App.css';
+import { Routes } from "./Routes";
 function App() {
-
   const [tweets, setTweets] = useState<string[]>([
-
     'Tweet 1',
     'Tweet 2',
     'Tweet 3',
     'Tweet 4',
-
-
   ])
-
 function createTweet(){
 setTweets([...tweets, 'Tweet 5'])
 }
-
   return (
-    <div>
 
-    {tweets.map(tweets =>{
-      return <Tweet text={tweets}/>
-    })}
+    <Routes/>
 
-     <button onClick={createTweet}>Adicionar Tweet</button>
-    </div>
+    // <div>
+    // {tweets.map(tweets =>{
+    //   return <Tweet text={tweets}/>
+    // })}
+    //  <button
+    //   onClick={createTweet}
+    //   style={{
+    //     backgroundColor: "blue",
+    //     border: 0,
+    //     padding: '6px 12px',
+    //     color: "white"
+    //   }} >
+    //    Adicionar Tweet</button>
+    // </div>
   );
 }
-
 export default App
