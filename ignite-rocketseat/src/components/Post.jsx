a413f5ee8723e6e8567fc9d41b516500df96a98d
Nathan-Delanhese <nnathanh3@gmail.com>
2022-06-18 10:26:04 -0300

    termino do dia 17

--- ignite-rocketseat/src/components/Post.jsx
+++ ignite-rocketseat/src/components/Post.jsx
@@ -1,8 +1,42 @@
 import React from 'react';
-
+import styles from './Post.module.css';
 
 const Post = () => {
-    return ( <p></p> );
+    return (
+
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/Nathan-Delanhese.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>
+                            Nathan Delanhese
+                        </strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+                <time title='17 de junho as 08:13' dateTime="2022-06-17 08:13:13">Publicado a 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p> Fala galeraa 👋</p>
+                <p>  Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p> 👉 {' '}<a href='#'>jane.design/doctorcare</a></p>
+                <p>   <a href='#'>#novoprojeto</a>{' '}
+                    <a href='#'>#nlw</a>{' '}
+                    <a href='#'>#rocketseat</a></p>
+
+            </div>
+
+<form className={styles.commentForm}>
+<strong>Deixe seu comentário</strong>
+<textarea placeholder='Deixe seu comentário'/>
+<button type='submit'>Comentar</button>
+</form>
+
+        </article>
+
+    );
 }
- 
+
 export default Post;
