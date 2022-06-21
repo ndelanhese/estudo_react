35778e2a214d31bbe9940d63eafaa38d718a5573
Nathan-Delanhese <nnathanh3@gmail.com>
2022-06-21 08:50:57 -0300

    iniciando o projeto dtmoney

---
+++ dt-money/src/App.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+font-size: 64px;
+color: #8257e6;
+`
+
+function App() {
+  return (
+    <div className="App">
+      <Title >Hello World</Title>
+    </div>
+  );
+}
+
+export default App;
