18084ce56d20f938db89c95a510dfd967c6e3058
Nathan-Delanhese <nnathanh3@gmail.com>
2022-06-21 16:38:32 -0300

    Fazendo componente de modal com react modal

--- dt-money/src/App.tsx
+++ dt-money/src/App.tsx
@@ -1,12 +1,35 @@
 import Dashboard from "./components/Dashboard/Dashboard";
 import Header from "./components/Header/Header";
 import { GlobalStyle } from "./styles/global";
+import Modal from 'react-modal';
+import { useState } from "react";
 
 function App() {
+
+  const [isNewTrasactionsModalOpen, setIsNewTrasactionsModalOpen] = useState(false);
+
+
+  function handleOpenNewTrasactionModal() {
+    setIsNewTrasactionsModalOpen(true);
+  }
+
+  function handleCloseNewTrasactionModal() {
+    setIsNewTrasactionsModalOpen(false)
+  }
+
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTrasactionModal} />
       <Dashboard />
+
+      <Modal
+        isOpen={isNewTrasactionsModalOpen}
+        onRequestClose={handleCloseNewTrasactionModal}>
+        <h2>Cadastrar transação</h2>
+      </Modal>
+
+
       <GlobalStyle />
     </>
   );
--- dt-money/src/components/Header/Header.tsx
+++ dt-money/src/components/Header/Header.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-const Header = () => {
+interface HeaderProps{
+    onOpenNewTransactionModal: () => void;
+}
+
+const Header = ({onOpenNewTransactionModal}: HeaderProps) => {
+
+  
+
     return (<Container>
         <Content>
             <img src={logoImg} alt="dt money" />
-            <button type="button">Nova Transação</button>
+            <button type="button" onClick={onOpenNewTransactionModal}>Nova Transação</button>
+            
         </Content>
     </Container>);
 }
