bb599bf9543a8ec79b6f1e16762e738e50433bf5
Nathan-Delanhese <nnathanh3@gmail.com>
2022-06-27 09:34:06 -0300

    inicio com o fauna bd

--- ignews/src/components/header/header.tsx
+++ ignews/src/components/header/header.tsx
@@ -2,11 +2,13 @@ import { SignInButton } from '../SignInButton/SignInButton';
 import styles from './styles.module.scss';
 
 export function Header() {
-    return (
+return (
 
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
+               <a >
                 <img src="/assets/images/logo.svg" alt="logo ignews"></img>
+                </a>
                 <nav>
                     <a className={styles.active}>Home</a>
                     <a>Posts</a>
