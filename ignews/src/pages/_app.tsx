f225dae0f78287d9a2523d1f036925998ab88574
ndelanhese <nnathanh3@gmail.com>
2022-06-27 16:19:29 -0300

    inicio fauna bd

--- ignews/src/pages/_app.tsx
+++ ignews/src/pages/_app.tsx
@@ -1,16 +1,15 @@
-
-import { Header } from "../components/Header/header";
-import { SessionProvider as NextAuthProvider } from "next-auth/react";
+import { Header } from "../components/Header/Header";
+import { SessionProvider } from "next-auth/react";
 import '../styles/global.scss';
 
 function MyApp({ Component,
    pageProps,
   }) {
   return (
-    <NextAuthProvider session={pageProps.session}>
+    <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </NextAuthProvider>
+    </SessionProvider>
   );
 }
 
--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,24 @@
 
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+import {fauna} from '../../../services/fauna';
+
+
+
 export default NextAuth({
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
-            
-        }),
-    ],
-    
+  }),
+],
+
+    callbacks: {
+        async signIn({ user, account, profile, email, credentials }) {
+          
+            return true
+        },
+        
+}
 })
