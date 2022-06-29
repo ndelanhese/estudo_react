549f4734278586f6ba8a6fdd0e74afd8cb6bcc6b
ndelanhese <nnathanh3@gmail.com>
2022-06-29 11:03:45 -0300

    inicio do retorno com status code do stripe

--- ignews/src/pages/api/subscribe.ts
+++ ignews/src/pages/api/subscribe.ts
@@ -1,29 +1,61 @@
-import { stripe } from './../../services/stripe';
+import { fauna } from './../../services/fauna';
+import { query as q } from 'faunadb';
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
-import { Stripe } from '../../../node_modules/stripe';
+
+type User = {
+    ref: {
+        id: string;
+
+    }
+    data: {
+        stripe_customer_id: string;
+    }
+}
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
 
     if (req.method === "POST") {
 
+        const stripe = require('stripe')(process.env.STIPE_API_KEY);
+
         const session = await getSession({ req });
 
 
-        const stripe = require('stripe')(process.env.STIPE_API_KEY);
+        const user = await fauna.query<User>(
+            q.Get(
+                q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(session.user.email)
+                )
+            )
+        )
 
-        
-        const stripeCustomer = await stripe.customers.create({
+        let customerId = user.data.stripe_customer_id;
 
-            email: session.user.email,
-        })
+        if (!customerId) {
 
+            const stripeCustomer = await stripe.customers.create({
+                email: session.user.email,
+            })
 
+            await fauna.query(
+                q.Update(
+                    q.Ref(
+                        q.Collection('users'), user.ref.id),
+                    {
+                        data: {
+                            stripe_customer_id: stripeCustomer.id,
+                        },
+                    },
+                )
+            )
 
+            customerId = stripeCustomer.id
 
+        }
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: stripeCustomer.id,
-
+            customer: customerId,
             payment_method_types: ['card'],
             billing_address_collection: 'required',
             line_items: [
@@ -40,9 +72,6 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
     } else {
         res.setHeader("Allow", "POST");
         res.status(405).end("Method Not Allowed");
-
     }
-
-
 
 };
