cb8569027750a8a3cf844459148bfe9c3cab2dfe
ndelanhese <nnathanh3@gmail.com>
2022-07-01 16:13:12 -0300

    Projeto pronto para build

--- ignews/src/services/prismic.ts
+++ ignews/src/services/prismic.ts
@@ -2,7 +2,7 @@ import * as prismic from "@prismicio/client";
 
 export function getPrismicCLient(req?: unknown) {
   const prismicToken = prismic.createClient(process.env.PRISMIC_ENDPOINT, {
-    req,
+  
     accessToken: process.env.PRISMIC_ACCESS_TOKEN,
   });
   return prismicToken;
