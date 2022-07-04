79a39dca4cb5c2427977b08ead35e39ec8267944
ndelanhese <nnathanh3@gmail.com>
2022-07-04 14:51:31 -0300

    tela de listagem de usuarios

--- dashgo/src/components/Header.tsx
+++ dashgo/src/components/Header.tsx
@@ -1,6 +1,10 @@
-import { Flex, Icon, Input, Text } from "@chakra-ui/react";
+import { Flex, Icon, Input, HStack, Text, Box, Avatar } from "@chakra-ui/react";
 import Link from "next/link";
-import { RiSearchLine } from "react-icons/ri";
+import {
+  RiNotificationLine,
+  RiSearchLine,
+  RiUserAddLine,
+} from "react-icons/ri";
 export function Header() {
   return (
     <Flex
@@ -46,7 +50,36 @@ export function Header() {
           px="4"
         />
 
-        <Icon as={RiSearchLine} fontSize="20"/>
+        <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr={4} textAlign="right">
+            <Text>Nathan Delanhese</Text>
+            <Text color="gray.300" fontSize="small">
+              ndelanhese@gmail.com
+            </Text>
+          </Box>
+          <Avatar
+            size="md"
+            name="Nathan Delanhese"
+            src="https://github.com/ndelanhese.png"
+          ></Avatar>
+        </Flex>
       </Flex>
     </Flex>
   );
--- dashgo/src/pages/dashboard.tsx
+++ dashgo/src/pages/dashboard.tsx
@@ -1,9 +1,88 @@
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
+const Chart = dynamic(() => import('react-apexcharts'), {
+  ssr: false,
+});
+//Carrega o componente dinamico somente no browser
 
-export default function Dashboard() {
-
-    return(
-        <Header></Header>
-    );
+const options = {chart: {
+  toolbar:{
+    show: false,
+  },
+  zoom:{
+    enable: false
+  }, 
+  foreColor: theme.colors.gray[500],
+},
+grid:{
+  show: false,
+},
+dataLabels:{
+  enabled:false
+},
+tooltip:{
+  enable:false
+}, 
+xaxis:{
+  type: 'datetime',
+  axiosBorder:{
+    color: theme.colors.gray[600]
+  },
+  axisTicks:{
+    color: theme.colors.gray[600]
+  },
+  categories:[
+    '2021-03-18T00:00:00.000Z',
+    '2021-03-19T00:00:00.000Z',
+    '2021-03-20T00:00:00.000Z',
+    '2021-03-21T00:00:00.000Z',
+    '2021-03-22T00:00:00.000Z',
+    '2021-03-23T00:00:00.000Z',
+    '2021-03-24T00:00:00.000Z',
+  ],
+},
+fill:{
+  opacity: 0.3,
+  type: 'gradient',
+  gradient: {
+    shade: 'dark',
+    opacityFrom: 0.7,
+    opacityTo: 0.3
+  }
 }
 
+};
+const series = [{ name: "series1", data: [31, 120, 10, 28, 61, 10, 59] }];
+
+export default function Dashboard() {
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+      <Flex w="100%" my={6} maxWidth={1480} mx="auto" px={6}>
+        <Sidebar />
+
+        <SimpleGrid
+          flex={1}
+          gap={4}
+          minChildWidth="320px" //Se for menor que 320px ela quebra linha
+          alignItems="flex-start"
+        >
+          <Box p={8} bg="gray.800" borderRadius={8} pb={4}>
+            <Text fontSize="large" mb={4}>
+              Week subscribes
+            </Text>
+            <Chart type="area" height={160} options={options} series={series} />
+          </Box>
+          <Box p={8} bg="gray.800" borderRadius={8}>
+            <Text fontSize="large" mb={4}>
+              Open rate
+            </Text>
+            <Chart type="area" height={160} options={options} series={series} />
+          </Box>
+        </SimpleGrid>
+      </Flex>
+    </Flex>
+  );
+}
