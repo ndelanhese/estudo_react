1c051e6584ea6d5e13874f1585bf6db69b2a186a
ndelanhese <nnathanh3@gmail.com>
2022-07-07 11:19:26 -0300

    hook de permissions ok

--- autentication/src/components/Dashboard.tsx
+++ autentication/src/components/Dashboard.tsx
@@ -5,8 +5,18 @@ import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
 import { AuthTokenError } from "../errors/AuthTokenError";
 import { destroyCookie } from "nookies";
+import { useCan } from "../hooks/useCan";
 export function Dashboard() {
   const { user } = useContext(AuthContext);
+
+  // verifica dentro do meu hook se o usuario tem as devidas permissions
+  const userCanSeeMetrics = useCan({
+    // Verifica se ele tem a permissions de metrics.list
+    permissions: ["metrics.list"],
+
+    // verifica se ele tem o cargo de ou editor ou administrator
+    roles: ["administrator", 'editor'],
+  });
 
   useEffect(() => {
     api
@@ -15,7 +25,14 @@ export function Dashboard() {
       .catch((err) => {});
   }, []);
 
-  return <h1>Dashboard: {user?.email}</h1>;
+  return (
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+
+      {/* se ele tem permissions, ele mostra */}
+      {userCanSeeMetrics && <div>Métricas</div>}
+    </>
+  );
 }
 
 // Método SSR (O getServerSideProps) é executado pelo lado do server antes de aparecer algo
